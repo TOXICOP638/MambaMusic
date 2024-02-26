ae009c24d2d35c1febbb428fcfa434aa3a7cd7d4
FantasticSukhi <122316561+FantasticSukhi@users.noreply.github.com>
2024-02-26 16:58:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN",None) 
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "harley Music Bot")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Mamba Music Bot")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -22,11 +22,11 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 
 # Chat id of a group for logging bot's activities
-#LOG_ID = int(getenv("LOG_ID", "-1001957871522"))
+#LOG_ID = int(getenv("LOG_ID", "-1001996954922"))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @RoseBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "2101893551"))
+OWNER_ID = int(getenv("OWNER_ID", "6713994904"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -36,15 +36,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Lippsxd/LippsMusicBot",
+    "https://github.com/FantasticSukhi/MambaMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "lippsxd")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "FantasticSukhi")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ThinkGrowMore")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ThinkGrowMore")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MAMBA_UPDATES")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MAMBA_HELPCHAT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -81,7 +81,7 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/7f6a403cd9874e6c9c38e.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/5b2f2e9e64ca6e84c75d7.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
